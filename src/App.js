2badecdb549e1403d328d60150d195003b4f7648
hooss <hoosshusu@gmail.com>
2020-06-23 16:39:29 +0900

    componets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
+    </div>
+  );
 }
 
 export default App;
