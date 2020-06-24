f88454fcbe3e07936f23e8dbeb4e016d3afb6a2b
hooss <hoosshusu@gmail.com>
2020-06-24 20:15:15 +0900

    Warm up

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating} / 5</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? `Loading...` : "We are ready"}</div>;
+  }
 }
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Sandwich",
-    image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2015/05/17/a38d367db0958c6ab7d29c6a9d4ab62a1.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Burger",
-    image:
-      "https://media-cdn.tripadvisor.com/media/photo-s/17/ba/a6/31/burger.jpg",
-    rating: 5,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F4%2F44%2FDolsot-bibimbap.jpg%2F1200px-Dolsot-bibimbap.jpg&f=1&nofb=1",
-    rating: 3,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
 export default App;
